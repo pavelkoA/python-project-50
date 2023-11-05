af1836cd68067f191962bab45093356a0a34c7c7
pavelkoA <pavelko88@mail.ru>
2023-11-05 10:13:49 +0300

    mod cli.py, add --format parametr

--- Makefile
+++ Makefile
@@ -14,4 +14,4 @@ package-reinstall:
 	python3 -m pip install --user --force-reinstall dist/*whl
 
 lint:
-	poetry run flake8 brain_games
+	poetry run flake8 gendiff
--- gendiff/cli.py
+++ gendiff/cli.py
@@ -6,4 +6,8 @@ def parse_command_arguments():
                                                 "files and shows a difference")
     parse.add_argument("first_file", type=str)
     parse.add_argument("second_file", type=str)
+    parse.add_argument("-f", "--format", type=str, help="set format of output")
     args = parse.parse_args()
+    return (args.first_file,
+            args.second_file,
+            args.format)
