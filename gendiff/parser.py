d2e3477cf624f3d8043d40685e0e5de83e24bb42
pavelkoA <pavelko88@mail.ru>
2023-11-09 17:46:04 +0300

    parser_codeclimatic

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -6,26 +6,26 @@ def get_json_file_dict(json_file):
         return json.load(file_read)
 
 
-def get_all_unique_and_sorted_keys(dict1, dict2):
+def get_all_unique_keys(dict1, dict2):
     unique_keys = set(dict1.keys()) | set(dict2.keys())
-    return sorted(unique_keys)
+    return unique_keys
 
 
 def get_result_string(first_file, second_file):
-    all_keys = get_all_unique_and_sorted_keys(first_file,
-                                              second_file)
+    all_keys = get_all_unique_keys(first_file,
+                                   second_file)
     result_string = "{\n"
-    for key in all_keys:
+    for key in sorted(all_keys):
         if key in first_file and key in second_file:
             if first_file[key] == second_file[key]:
-                result_string += f"    {key}: {first_file[key]}\n"
+                result_string += f"  {key}: {first_file[key]}\n"
             else:
-                result_string += f"  - {key}: {first_file[key]}\n"
-                result_string += f"  + {key}: {second_file[key]}\n"
-        elif key in first_file and key not in second_file:
-            result_string += f"  - {key}: {first_file[key]}\n"
-        elif key not in first_file and key in second_file:
-            result_string += f"  + {key}: {second_file[key]}\n"
+                result_string += f"- {key}: {first_file[key]}\n"
+                result_string += f"+ {key}: {second_file[key]}\n"
+        elif key in first_file:
+            result_string += f"- {key}: {first_file[key]}\n"
+        elif key in second_file:
+            result_string += f"+ {key}: {second_file[key]}\n"
     result_string += "}"
     return result_string
 
@@ -34,3 +34,19 @@ def diff_generate(first_file, second_file):
     diff_result_string = get_result_string(get_json_file_dict(first_file),
                                            get_json_file_dict(second_file))
     return diff_result_string
+
+
+first_file = {
+  "host": "hexlet.io",
+  "timeout": 50,
+  "proxy": "123.234.53.22",
+  "follow": False
+}
+
+second_file = {
+  "timeout": 20,
+  "verbose": True,
+  "host": "hexlet.io"
+}
+
+print(get_result_string(first_file, second_file))
