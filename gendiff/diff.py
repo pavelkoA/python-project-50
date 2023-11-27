bc10ae61e8f77f95f119d21235d965bd0358d7c4
pavelkoA <pavelko88@mail.ru>
2023-11-27 21:18:30 +0300

    formater restructured

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -1,44 +1,38 @@
 from gendiff.parser import get_utils_to_readerd
-from gendiff.formatter.formatter import stringify_diff
-
-
-def is_recursive_value(first_value, second_value):
-    return all([isinstance(first_value, dict),
-                isinstance(second_value, dict)])
+from gendiff.formatter.formatter import stringify
 
 
 def get_diff_dict(first_file, second_file):
     keys = first_file.keys() | second_file.keys()
-    result = []
+    result = {}
     for key in sorted(keys):
         temp_dict = {"key": key}
         first_value = first_file.get(key)
         second_value = second_file.get(key)
         if key not in first_file:
-            temp_dict["type"] = "deleted"
-            temp_dict["value"] = first_value
+            result[key] ={"type": "deleted",
+                          "value": second_value}
         elif key not in second_file:
-            temp_dict["type"] = "added"
-            temp_dict["value"] = second_value
-        elif is_recursive_value(first_value,
-                                second_value):
-            temp_dict["type"] = "other"
-            temp_dict["value"] = get_diff_dict(first_value,
-                                               second_value)
+            result[key] ={"type": "added",
+                          "value": first_value}
+        elif all([isinstance(first_value, dict),
+                  isinstance(second_value, dict)]):
+            result[key] ={"type": "other",
+                          "children": get_diff_dict(first_value,
+                                                    second_value)}
         elif first_value == second_value:
-            temp_dict["type"] = "unchanged"
-            temp_dict["value"] = first_value
+            result[key] ={"type": "unchanged",
+                          "value": first_value}
         else:
-            temp_dict["type"] = "changed"
-            temp_dict["old_value"] = first_value
-            temp_dict["new_value"] = second_value
-        result.append(temp_dict)
+            result[key] ={"type": "changed",
+                         "old_value": first_value,
+                         "new_value": second_value}
     return result
 
 
 def diff_generate(first_file, second_file):
     file_1_json_to_dict = get_utils_to_readerd(first_file)
     file_2_json_to_dict = get_utils_to_readerd(second_file)
-    diff_dict = get_diff_dict(file_1_json_to_dict,
-                              file_2_json_to_dict)
-    return stringify_diff(diff_dict)
+    diff = get_diff_dict(file_1_json_to_dict,
+                         file_2_json_to_dict)
+    return stringify(diff)
--- gendiff/formatter/formatter.py
+++ gendiff/formatter/formatter.py
@@ -1,50 +1,18 @@
 import json
-import itertools
 
 
-def generate_string(string_data_dict, depth):
-    type = string_data_dict["type"]
-    match type:
-        case "added":
-            return f"{depth}+ {string_data_dict['key']}:" \
-                   f" {string_data_dict.get('value')}\n"
-        case "removed":
-            return f"{depth}- {string_data_dict['key']}:" \
-                   f" {string_data_dict.get('value')}\n"
-        case "unchanged":
-            return f"{depth}  {string_data_dict['key']}:" \
-                   f" {string_data_dict.get('value')}\n"
-        case "changed":
-            return f"{depth}- {string_data_dict['key']}:" \
-                   f" {string_data_dict.get('old_value')}\n" \
-                   f"{depth}+ {string_data_dict['key']}:" \
-                   f" {string_data_dict.get('new_value')}\n"
-
-
-def stringify_diff(data_dict, replacer=" ", spaces_count=1):
-
-    def walk_the_dict(current_value, depth):
-        deep_indent_size = depth + spaces_count
-        deep_indent = replacer * deep_indent_size
-        current_indent = replacer * depth
-        lines = []
-        for row in current_value:
-            if row["type"] != "other":
-                lines.append(generate_string(row, deep_indent))
-            else:
-                lines.append(generate_string(
-                             walk_the_dict(row["value"],
-                                           deep_indent_size), deep_indent))
-        result = itertools.chain("{", lines, [current_indent + "}"])
-        return "\n".join(result)
-
-    return walk_the_dict(data_dict, 0)
+def stringify(diff, depth=1):
+    replacer = "  "
+    result = []
+    for key, value in diff.items():
+        if value["type"] == "added":
+            result.append(f"{replacer * depth}- " \
+                          f"{key}: {value['value']}")
+    return "".join(["{\n", *result, "\n}"])
 
 
 
 
-# def conversion_to_json_format(diff_dict):
-#     json_string = json.dumps(diff_dict, indent=2)
-#     json_string = json_string.replace('"', '')
-#     json_string = json_string.replace(",", "")
-#     return json_string
+# data = {"key1": {"type": "added",
+#                  "value": "VALUE"}}
+# print(stringify(data))
