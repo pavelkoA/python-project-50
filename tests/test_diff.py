fb43335b1c8ce695f5dc6f8ea67c398953e3aef7
pavelkoA <pavelko88@mail.ru>
2023-11-29 18:26:20 +0300

    fix tests

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -12,13 +12,7 @@ def test_get_diff_dict(dict_1,dict_2,result_dict):
     assert get_diff_dict(dict_1, dict_2) == result_dict
 
 
-# @pytest.mark.parametrize("data_dict, result_string",
-#                          data_file.test_data_generate_string)
-# def test_generate_string(data_dict, result_string):
-#     assert generate_string(data_dict) == result_string
-
-
-# @pytest.mark.parametrize("path_file1, path_file22, result_string",
-#                          data_file.test_data_diff_generate)
-# def test_diff_generate(path_file1, path_file22, result_string):
-#     assert diff_generate(path_file1, path_file22) == result_string
+@pytest.mark.parametrize("path_file1, path_file22, result_string",
+                         data_file.test_data_diff_generate)
+def test_diff_generate(path_file1, path_file22, result_string):
+    assert diff_generate(path_file1, path_file22) == result_string
