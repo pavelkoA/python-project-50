5910a95844cfa5f2eae1786b3556c4b307a97a2b
pavelkoA <pavelko88@mail.ru>
2023-12-20 09:40:06 +0300

    ref3

--- gendiff/formatter/stylish.py
+++ gendiff/formatter/stylish.py
@@ -16,29 +16,30 @@ def to_str(data, indent):
     return str(data)
 
 
-def construct_stylish_diff(diff, depth=0):
-    result = []
-    indent = "  "
-    indent += NEXT_INDENT * depth
-    for key, data in diff.items():
-        match data["type"]:
-            case "changed":
-                old_value = to_str(data['old_value'], indent)
-                new_value = to_str(data['new_value'], indent)
-                added_symbol, deleted_symbol = SPECIAL_SYMBOL["changed"]
-                result.append(f"{indent}{added_symbol}"
-                              f"{key}: {old_value}\n"
-                              f"{indent}{deleted_symbol}"
-                              f"{key}: {new_value}")
-            case "nested":
-                children = construct_stylish_diff(data["children"],
-                                                  depth + 1)
-                nested_symbol = SPECIAL_SYMBOL["nested"]
-                result.append(f"{indent}{nested_symbol}"
-                              f"{key}: {children}")
-            case _:
-                value = to_str(data['value'], indent)
-                sign = SPECIAL_SYMBOL[data["type"]]
-                result.append(f"{indent}{sign}"
-                              f"{key}: {value}")
-    return "\n".join(["{", *result, indent[:-2] + "}"])
+def construct_stylish_diff(diff):
+
+    def iter_by_diff(diff, indent="  ", depth=0):
+        result = []
+        for key, data in diff.items():
+            match data["type"]:
+                case "changed":
+                    old_value = to_str(data['old_value'], indent)
+                    new_value = to_str(data['new_value'], indent)
+                    added_symbol, deleted_symbol = SPECIAL_SYMBOL["changed"]
+                    result.append(f"{indent}{added_symbol}"
+                                  f"{key}: {old_value}\n"
+                                  f"{indent}{deleted_symbol}"
+                                  f"{key}: {new_value}")
+                case "nested":
+                    children = iter_by_diff(data["children"],
+                                            indent + NEXT_INDENT * (depth + 1))
+                    nested_symbol = SPECIAL_SYMBOL["nested"]
+                    result.append(f"{indent}{nested_symbol}"
+                                  f"{key}: {children}")
+                case _:
+                    value = to_str(data['value'], indent)
+                    sign = SPECIAL_SYMBOL[data["type"]]
+                    result.append(f"{indent}{sign}"
+                                  f"{key}: {value}")
+        return "\n".join(["{", *result, indent[:-2] + "}"])
+    return iter_by_diff(diff)
