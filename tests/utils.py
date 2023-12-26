6b4aa354008c0bf6f31c7428525a0df22cfa3cf6
pavelkoA <pavelko88@mail.ru>
2023-12-26 12:18:20 +0300

    ref4

--- tests/utils.py
+++ tests/utils.py
@@ -1,11 +1,14 @@
 import os
 
 
-def read_test_files(file1, file2, file_result_name):
+def get_test_path(file1, file2):
     path_file1 = f"{os.getcwd()}/tests/fixtures/" + file1
     path_file2 = f"{os.getcwd()}/tests/fixtures/" + file2
+    return path_file1, path_file2
+
+
+def read_result_files(file_result_name):
     path_result = f"{os.getcwd()}/tests/fixtures/results/"
     with open(path_result + file_result_name, "r",
               encoding="utf-8") as result_file:
-        result = result_file.read()
-    return path_file1, path_file2, result
+        return result_file.read()
